0e53d05cd1585c89f57349e7d8872fe5e525e562
Emma Carpenetti <emmac4164@gmail.com>
2022-07-16 14:08:59 -0400

    Initial Commit

---
+++ ARAppTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ARAppTest
+//
+//  Created by Emma Carpenetti on 7/16/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
